2e2947dc8fd2916f55fe1dbd473494ad471b321b
Ee Ellisa <eee92156@gmail.com>
2025-02-10 23:56:05 +0100

    added function getAllProjects

--- api/ProjectDAO.php
+++ api/ProjectDAO.php
@@ -7,5 +7,29 @@ class ProjectDAO{
     }
     public function getAllProjects(){
         $query = "SELECT * FROM project";
+        $req = $this->linkpdo->prepare($query);
+        $req->execute();
+        $arr = array();
+        while($donnees=$req->fetch()){
+            $date_debut = date('Y-m',$donnees['date_debut']);
+            $date_fin = date('Y-m',$donnees['date_fin']);
+            $queryTags = "SELECT t.title FROM project_tag p, tag t WHERE p.id_tag=t.id_tag AND p.id_project=:id";
+            $reqTags = $this->linkpdo->prepare($queryTags);
+            $reqTags->execute(array('id'=>$donnees['id_project']));
+            $arrTags = array();
+            while($donneesTags=$reqTags->fetch()){
+                array_push($arrTags,$donneesTags['title']);
+            }
+            $arr[]=array(
+                'id'=>$donnees['id_project'],
+                'title'=>$donnees['title'],
+                'description'=>$donnees['description'],
+                'photo'=>$donnees['photo'],
+                'site_project'=>$donnees['site_project'],
+                'date_debut'=>$date_debut,
+                'date_fin'=>$date_fin,
+                'tags'=>$arrTags
+            );
+        }
     }
 }
